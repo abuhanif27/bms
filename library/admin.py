d8d79d65b00a21aebc3864fc9cc111770d7f0621
Abu Hanif <ahanif2500@gmail.com>
2025-03-05 14:43:09 +0600

    admin panel customization

--- library/admin.py
+++ library/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Books
 
+admin.site.site_header = "BMS Administration"
+admin.site.site_title = "BMS Admin Portal"
+admin.site.index_title = "Welcome to the BMS Dashboard"
+
 @admin.register(Books)
 class BooksAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'genre', 'published_date', 'added_on')
